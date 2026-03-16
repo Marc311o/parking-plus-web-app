921b4fd679df92ae00ef4780b6db6ee7f74a6b73
unknown <kbanaszewskaa@gmail.com>
2026-03-16 10:43:42 +0100

    ORM for vehicles module + maven configuration

---
+++ backend/src/main/kotlin/com/parkingplus/vehicles/VehicleController.kt
@@ -0,0 +1,5 @@
+package com.parkingplus.vehicles
+
+class VehicleController {
+    //TODO: Implement RESTful endpoints for vehicle management, such as creating a new vehicle, updating vehicle information, and retrieving vehicles by owner or license plate.
+}
