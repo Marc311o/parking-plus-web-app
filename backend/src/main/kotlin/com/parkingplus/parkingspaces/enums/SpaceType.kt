7ee918fe82a9f663b9aa38a43a0abc2e2b123a7f
Wojciech Korbel <251169@edu.p.lodz.pl>
2026-03-16 12:23:49 +0100

    Added SpaceType enum

---
+++ backend/src/main/kotlin/com/parkingplus/parkingspaces/enums/SpaceType.kt
@@ -0,0 +1,10 @@
+package com.parkingplus.vehicles.enums
+
+enum class SpaceType {
+    REGULAR_ABLEBODIED,
+    REGULAR_HANDICAPED,
+    EV_ABLEBODIED,
+    EV_HANDICAPED,
+    REGULAR_BOTH,
+    EV_BOTH
+}
