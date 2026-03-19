9e14d97d80a1bda6f361be1defa9612dfd2eee56
unknown <kbanaszewskaa@gmail.com>
2026-03-19 18:22:41 +0100

    Fixed files

--- backend/src/main/kotlin/com/parkingplus/tariffs/TariffRepository.kt
+++ backend/src/main/kotlin/com/parkingplus/tariffs/TariffRepository.kt
@@ -3,7 +3,7 @@ package com.parkingplus.tariffs
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface TariffRepository : JpaRepository<TariffEntity, Long> {
-    fun findAllByIsDailyTrue(isDaily: Boolean): List<TariffEntity>
+    fun findAllByIsDaily(isDaily: Boolean): List<TariffEntity>
 //    szuka pojedynczego wiersza z bazy (istotne na później)
     fun findByIsDailyAndDayOfWeekAndStartHourAndEndHour(isDaily: Boolean, dayOfWeek: String, startHour: Int, endHour: Int): List<TariffEntity>
 }
