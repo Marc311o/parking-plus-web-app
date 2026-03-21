0b3a899b45bb15dbe56dc290807b544eb4524882
Jerzy Andrusiewicz <jurek.andrusiewicz@gmail.com>
2026-03-21 21:37:30 +0100

    fix

--- backend/src/main/kotlin/com/parkingplus/ParkingPlusApplication.kt
+++ backend/src/main/kotlin/com/parkingplus/ParkingPlusApplication.kt
@@ -2,10 +2,23 @@ package com.parkingplus
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import io.github.cdimascio.dotenv.Dotenv
 
 @SpringBootApplication
 open class ParkingPlusApplication
 
 fun main(args: Array<String>) {
+
+
+    val dotenv = Dotenv.configure()
+        .directory("./")
+        .ignoreIfMalformed()
+        .ignoreIfMissing()
+        .load()
+
+    dotenv.entries().forEach { entry ->
+        System.setProperty(entry.key, entry.value)
+    }
+
     runApplication<ParkingPlusApplication>(*args)
 }
