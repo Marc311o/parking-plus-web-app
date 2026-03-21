5ca71d59489832aace9e8854c658993ec1924c75
Wojciech Korbel <251169@edu.p.lodz.pl>
2026-03-21 11:12:59 +0100

    Type fix in TransactionDTO

--- backend/src/main/kotlin/com/parkingplus/transactions/TransactionDTO.kt
+++ backend/src/main/kotlin/com/parkingplus/transactions/TransactionDTO.kt
@@ -4,7 +4,7 @@ import com.parkingplus.transactions.enums.TransactionType
 import java.time.LocalDateTime
 
 data class TransactionDTO(
-    val id: Long? = null,
+    val id: Int? = null,
     val userId: Long,
     val type: TransactionType,
     val amount: Float,
