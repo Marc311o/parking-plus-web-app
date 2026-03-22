2e3445571e3d492b1df2786c1529acbd65715616
Wojciech Korbel <157359659+WojciechKorbel@users.noreply.github.com>
2026-03-22 21:05:27 +0100

    Domain for parkingspaces (#34)
    * Added ParkingSpaceDTO
    
    * Added ParkingSpaceMapper
    
    * Bug fix in ParkingSpaceRepository
    
    * Bug fix in ParkingSpaceMapper
    
    * Added ParkingSpaceService and ParkingSpaceController
    
    * Deleted gitkeep
    
    * Added validation in controller
    
    * Refactored put in controller

--- backend/src/main/kotlin/com/parkingplus/parkingspaces/ParkingSpaceDTO.kt
+++ backend/src/main/kotlin/com/parkingplus/parkingspaces/ParkingSpaceDTO.kt
@@ -1,5 +1,19 @@
 package com.parkingplus.parkingspaces
 
-class ParkingSpaceDTO {
-    // TODO:
-}
+import com.parkingplus.parkingspaces.enums.ParkingSpaceStatus
+import com.parkingplus.parkingspaces.enums.SpaceType
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+
+data class ParkingSpaceDTO (
+    @field:NotBlank(message = "Parking space ID is required")
+    val id: String,
+
+    @field:NotNull(message = "Status is required")
+    val status: ParkingSpaceStatus,
+
+    @field:NotNull(message = "Space type is required")
+    val spaceType: SpaceType,
+
+    val level: Int
+)
