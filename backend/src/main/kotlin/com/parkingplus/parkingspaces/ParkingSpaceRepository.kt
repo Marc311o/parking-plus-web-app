82a62c0651bc9c8f5402d2f04edf18ffdfd584d1
Jerzy Andrusiewicz <155088776+Marc311o@users.noreply.github.com>
2026-04-30 09:30:30 +0200

    Parking spaces current occupancy stats endpoint
    * implement parking spaces current occupancy stats endpoint
    
    * performance fix

--- backend/src/main/kotlin/com/parkingplus/parkingspaces/ParkingSpaceRepository.kt
+++ backend/src/main/kotlin/com/parkingplus/parkingspaces/ParkingSpaceRepository.kt
@@ -3,6 +3,7 @@ package com.parkingplus.parkingspaces
 import com.parkingplus.parkingspaces.enums.SpaceType
 import org.springframework.data.jpa.repository.JpaRepository
 import com.parkingplus.parkingspaces.enums.ParkingSpaceStatus
+import org.springframework.data.jpa.repository.Query
 
 
 interface ParkingSpaceRepository : JpaRepository<ParkingSpaceEntity, String> {
@@ -10,4 +11,7 @@ interface ParkingSpaceRepository : JpaRepository<ParkingSpaceEntity, String> {
     fun findAllBySpaceType(spaceType: SpaceType): List<ParkingSpaceEntity>
     fun findAllByStatus(status: ParkingSpaceStatus): List<ParkingSpaceEntity>
     fun findAllByLevelAndSpaceType(level: Int, spaceType: SpaceType): List<ParkingSpaceEntity>
+
+    @Query("SELECT p.status, COUNT(p) FROM ParkingSpaceEntity p GROUP BY p.status")
+    fun countByStatusGrouped(): List<Array<Any>>
 }
