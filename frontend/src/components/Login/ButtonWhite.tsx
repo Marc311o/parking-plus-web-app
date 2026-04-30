fbbd008acbc5ff71053d9717b9290e004b569007
Wojciech Korbel <251169@edu.p.lodz.pl>
2026-04-30 20:41:11 +0200

    Removed css from LoginPage

--- frontend/src/components/Login/ButtonWhite.tsx
+++ frontend/src/components/Login/ButtonWhite.tsx
@@ -21,6 +21,8 @@ export function ButtonWhite({
             disabled={disabled}
             variant="outlined"
             sx={{
+                width: "200px",
+                fontSize: "20px",
                 color: "#8b1f9e",
                 background: "#ffffff",
                 border: "3px solid #8b1f9e",
