c6f0abb21311cdcbcd1856326f60f2b5e0bdcc78
Wojciech Korbel <251169@edu.p.lodz.pl>
2026-05-17 21:03:14 +0200

    Init MyCarsPage

---
+++ frontend/src/pages/MyCarsPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const MyCarsPage = () => {
+  return (
+    <div className="my-cars-page">
+      <h1>My Cars</h1>
+
+      <div className="content">
+        {/* placeholder */}
+      </div>
+    </div>
+  );
+};
+
+export default MyCarsPage;
